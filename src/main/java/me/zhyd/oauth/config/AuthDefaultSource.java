5d1b701427030318adcd744093be220bf65dd2a0
chenlei <poison7@yeah.net>
2019-12-13 17:56:09 +0800

    111

--- src/main/java/me/zhyd/oauth/config/AuthDefaultSource.java
+++ src/main/java/me/zhyd/oauth/config/AuthDefaultSource.java
@@ -258,6 +258,27 @@ public enum AuthDefaultSource implements AuthSource {
             return "https://api.weixin.qq.com/sns/oauth2/refresh_token";
         }
     },
+    WECHATMP {
+        @Override
+        public String authorize() {
+            return "https://open.weixin.qq.com/connect/oauth2/authorize";
+        }
+
+        @Override
+        public String accessToken() {
+            return "https://api.weixin.qq.com/sns/oauth2/access_token";
+        }
+
+        @Override
+        public String userInfo() {
+            return "https://api.weixin.qq.com/sns/userinfo";
+        }
+
+        @Override
+        public String refresh() {
+            return "https://api.weixin.qq.com/sns/oauth2/refresh_token";
+        }
+    },
     /**
      * 淘宝
      */
